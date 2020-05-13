667ed8b491b181d49a8f3345396019ba8a2adada
Hodosov <Hodosov.y@mail.ru>
2020-05-13 14:30:36 +0300

    edit paginator

--- src/components/Paginator/paginator.jsx
+++ src/components/Paginator/paginator.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export let Pagination = ({totalUsersCount, pageSize, onPageChanged}) => {
 
@@ -9,10 +9,24 @@ export let Pagination = ({totalUsersCount, pageSize, onPageChanged}) => {
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
+    
+    let portionSize = 10
+    let portionCount = Math.ceil(pagesCount/portionSize)
+
+    let [portionNumber, setPortionNumber] = useState(1)
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    let rightPortionPageNumber = portionNumber * portionSize
+
+    console.log('render')
 
     return (
         <div>
-            {pages.map(p => <button onClick={() => { onPageChanged(p) }}>{p}</button>)}
+            {portionNumber >1  && <button 
+            onClick={() => setPortionNumber(portionNumber - 1)}>Left</button>}
+            {pages.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map(p => <button onClick={() => { onPageChanged(p) }}>{p}</button>)}
+            {portionCount > portionNumber && <button
+            onClick={() => setPortionNumber(portionNumber + 1)}>Right</button>}
         </div>
     )
 }
